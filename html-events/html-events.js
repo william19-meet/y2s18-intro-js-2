f87d55be1dc309fd2761123eefc4adb1f319fdf7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:33:47 +0300

    finally something good

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,14 @@
 // Put all your JavaScript in this file!
+//function changeColor(){
+//	var x =prompt("what color do you want?")
+function myFunction() {
+	var y = document.getElementById("myAudio");
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.play();
+    } else {
+        x.style.display = "none";
+    }
+}
+
