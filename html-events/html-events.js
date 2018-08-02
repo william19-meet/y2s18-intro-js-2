1f194944ab0ea177fcc99dfd03f77635f662b9a2
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:31:55 +0300

    hahha

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -4,11 +4,17 @@
 function myFunction() {
 	var y = document.getElementById("myAudio");
     var x = document.getElementById("myDIV");
+    var z = document.getElementById("mytext");
+    var b = document.getElementsByTagName("body");
     if (x.style.display === "none") {
         x.style.display = "block";
         y.play();
     } else {
         x.style.display = "none";
     }
-}
 
+    if (z.style.display === "block") {
+        z.style.display = "none";
+    }
+    b.style.backgroundColor = "black"
+}
